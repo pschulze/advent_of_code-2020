46ee5a11a18859f6ef9b7b6fa24a34c5652d207e
Peter Schulze <peterkschulze@gmail.com>
2020-12-05 19:14:58 -0700

    day 1

---
+++ day_01.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+entries = File.readlines(File.join('fixtures', 'day_01.txt')).map(&:to_i)
+
+[2, 3].each do |n|
+  entries.combination(n) { |combo| puts "#{n} Entry Answer: #{combo.inject(:*)}" if combo.sum == 2020 }
+end
